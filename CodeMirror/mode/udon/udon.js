805913b35218969881a7db5d6c22aa429b31c4e3
John Franklin <jfranklin9000@gmail.com>
2019-08-23 06:33:56 -0500

    delete stuff: allowAtxHeaderWithoutSpace, commented out markdown code for bold and italics, linkTitle stuff (was part of footnote stuff), setext

--- CodeMirror/mode/udon/udon.js
+++ CodeMirror/mode/udon/udon.js
@@ -64,14 +64,10 @@ CodeMirror.defineMode("udon", function(cmCfg, modeCfg) {
 //,   listRE = /^(?:[*\-+]|^[0-9]+([.)]))\s+/ // markdown
   ,   listRE = /^(?:[\-+]|^[0-9]+([.)]))\s+/  // udon
       //
-      // changed - require at least one space to be a header
-//,   atxHeaderRE = modeCfg.allowAtxHeaderWithoutSpace ? /^(#+)/ : /^(#+)(?: |$)/    // markdown
-//,   atxHeaderRE = modeCfg.allowAtxHeaderWithoutSpace ? /^(#+)/ : /^(#+)(?: (?! ))/ // udon - only one space
-  ,   atxHeaderRE = modeCfg.allowAtxHeaderWithoutSpace ? /^(#+)/ : /^(#+)(?: )/      // udon - at least one space
-      //
-      // changed - don't allow === or --- to headerize the previous line
-//,   setextHeaderRE = /^ *(?:\={1,}|-{1,})\s*$/ // markdown
-  ,   setextHeaderRE = /(?!)/                    // udon - matches nothing
+      // changed - require at least one space to be a header, and hax newline is not a header
+//,   atxHeaderRE = modeCfg.allowAtxHeaderWithoutSpace ? /^(#+)/ : /^(#+)(?: |$)/ // markdown
+//,   atxHeaderRE = /^(#+)(?: (?! ))/                                             // udon - only one space
+  ,   atxHeaderRE = /^(#+)(?: )/                                                  // udon - at least one space
       //
       // no change
   ,   textRE = /^[^#!\[\]*_\\<>` "'(~:]+/
@@ -80,10 +76,6 @@ CodeMirror.defineMode("udon", function(cmCfg, modeCfg) {
 //,   fencedCodeRE = /^(~~~+|```+)[ \t]*([\w+#-]*)[^\n`]*$/ // markdown
   ,   fencedCodeRE = /^(```)(.*)$/                          // udon - match more than ``` so we can error on ```json
       //
-      // no change
-  ,   linkDefRE = /^\s*\[[^\]]+?\]:.*$/ // naive link-definition
-      // no change
-  ,   punctuation = /[!"#$%&'()*+,\-.\/:;<=>?@\[\\\]^_`{|}~\xA1\xA7\xAB\xB6\xB7\xBB\xBF\u037E\u0387\u055A-\u055F\u0589\u058A\u05BE\u05C0\u05C3\u05C6\u05F3\u05F4\u0609\u060A\u060C\u060D\u061B\u061E\u061F\u066A-\u066D\u06D4\u0700-\u070D\u07F7-\u07F9\u0830-\u083E\u085E\u0964\u0965\u0970\u0AF0\u0DF4\u0E4F\u0E5A\u0E5B\u0F04-\u0F12\u0F14\u0F3A-\u0F3D\u0F85\u0FD0-\u0FD4\u0FD9\u0FDA\u104A-\u104F\u10FB\u1360-\u1368\u1400\u166D\u166E\u169B\u169C\u16EB-\u16ED\u1735\u1736\u17D4-\u17D6\u17D8-\u17DA\u1800-\u180A\u1944\u1945\u1A1E\u1A1F\u1AA0-\u1AA6\u1AA8-\u1AAD\u1B5A-\u1B60\u1BFC-\u1BFF\u1C3B-\u1C3F\u1C7E\u1C7F\u1CC0-\u1CC7\u1CD3\u2010-\u2027\u2030-\u2043\u2045-\u2051\u2053-\u205E\u207D\u207E\u208D\u208E\u2308-\u230B\u2329\u232A\u2768-\u2775\u27C5\u27C6\u27E6-\u27EF\u2983-\u2998\u29D8-\u29DB\u29FC\u29FD\u2CF9-\u2CFC\u2CFE\u2CFF\u2D70\u2E00-\u2E2E\u2E30-\u2E42\u3001-\u3003\u3008-\u3011\u3014-\u301F\u3030\u303D\u30A0\u30FB\uA4FE\uA4FF\uA60D-\uA60F\uA673\uA67E\uA6F2-\uA6F7\uA874-\uA877\uA8CE\uA8CF\uA8F8-\uA8FA\uA8FC\uA92E\uA92F\uA95F\uA9C1-\uA9CD\uA9DE\uA9DF\uAA5C-\uAA5F\uAADE\uAADF\uAAF0\uAAF1\uABEB\uFD3E\uFD3F\uFE10-\uFE19\uFE30-\uFE52\uFE54-\uFE61\uFE63\uFE68\uFE6A\uFE6B\uFF01-\uFF03\uFF05-\uFF0A\uFF0C-\uFF0F\uFF1A\uFF1B\uFF1F\uFF20\uFF3B-\uFF3D\uFF3F\uFF5B\uFF5D\uFF5F-\uFF65]|\uD800[\uDD00-\uDD02\uDF9F\uDFD0]|\uD801\uDD6F|\uD802[\uDC57\uDD1F\uDD3F\uDE50-\uDE58\uDE7F\uDEF0-\uDEF6\uDF39-\uDF3F\uDF99-\uDF9C]|\uD804[\uDC47-\uDC4D\uDCBB\uDCBC\uDCBE-\uDCC1\uDD40-\uDD43\uDD74\uDD75\uDDC5-\uDDC9\uDDCD\uDDDB\uDDDD-\uDDDF\uDE38-\uDE3D\uDEA9]|\uD805[\uDCC6\uDDC1-\uDDD7\uDE41-\uDE43\uDF3C-\uDF3E]|\uD809[\uDC70-\uDC74]|\uD81A[\uDE6E\uDE6F\uDEF5\uDF37-\uDF3B\uDF44]|\uD82F\uDC9F|\uD836[\uDE87-\uDE8B]/
       // no change
   ,   expandedTab = "    " // CommonMark specifies tab as 4 spaces
 // ~udon end
@@ -114,9 +106,9 @@ CodeMirror.defineMode("udon", function(cmCfg, modeCfg) {
 //  return null;
     // Reset udonParseError state
     state.udonParseError = false; // ~udon
-    // Reset linkTitle state
-    state.linkTitle = false;
+    // Reset linkHref state
     state.linkHref = false;
+     // Reset linkText state
     state.linkText = false;
     // Reset EM state
     state.em = false;
@@ -177,13 +169,6 @@ CodeMirror.defineMode("udon", function(cmCfg, modeCfg) {
       }
     }
 
-    // not comprehensive (currently only for setext detection purposes)
-    var allowsInlineContinuation = (
-        !prevLineLineIsEmpty && !prevLineIsHr && !state.prevLine.header &&
-        (!prevLineIsList || !prevLineIsIndentedCode) &&
-        !state.prevLine.fencedCodeEnd
-    );
-
     var isHr = (state.list === false || prevLineIsHr || prevLineLineIsEmpty) &&
       state.indentation <= maxNonCodeIndentation && stream.match(hrRE);
 
@@ -210,7 +195,7 @@ CodeMirror.defineMode("udon", function(cmCfg, modeCfg) {
       if (modeCfg.highlightFormatting) state.formatting = "quote";
       stream.eatSpace();
       return getType(state);
-    } else if (!isHr && !state.setext && firstTokenOnLine && state.indentation <= maxNonCodeIndentation && (match = stream.match(listRE))) {
+    } else if (!isHr && firstTokenOnLine && state.indentation <= maxNonCodeIndentation && (match = stream.match(listRE))) {
       var listType = match[1] ? "ol" : "ul";
 
       state.indentation = lineIndentation + stream.current().length;
@@ -233,30 +218,6 @@ CodeMirror.defineMode("udon", function(cmCfg, modeCfg) {
       state.f = state.block = local;
       if (modeCfg.highlightFormatting) state.formatting = "code-block";
       state.code = -1
-      return getType(state);
-    // SETEXT has lowest block-scope precedence after HR, so check it after
-    //  the others (code, blockquote, list...)
-    } else if (
-      // if setext set, indicates line after ---/===
-      state.setext || (
-        // line before ---/===
-        (!allowsInlineContinuation || !prevLineIsList) && !state.quote && state.list === false &&
-        !state.code && !isHr && !linkDefRE.test(stream.string) &&
-        (match = stream.lookAhead(1)) && (match = match.match(setextHeaderRE))
-      )
-    ) {
-      if ( !state.setext ) {
-        state.header = match[0].charAt(0) == '=' ? 1 : 2;
-        state.setext = state.header;
-      } else {
-        state.header = state.setext;
-        // has no effect on type so we can reset it now
-        state.setext = 0;
-        stream.skipToEnd();
-        if (modeCfg.highlightFormatting) state.formatting = "header";
-      }
-      state.thisLine.header = true;
-      state.f = state.inline;
       return getType(state);
     } else if (isHr) {
       stream.skipToEnd();
@@ -394,20 +355,6 @@ CodeMirror.defineMode("udon", function(cmCfg, modeCfg) {
 
     var ch = stream.next();
 
-    // Matches link titles present on next line
-    if (state.linkTitle) {
-      state.linkTitle = false;
-      var matchCh = ch;
-      if (ch === '(') {
-        matchCh = ')';
-      }
-      matchCh = (matchCh+'').replace(/([.?*+^\[\]\\(){}|-])/g, "\\$1");
-      var regex = '^\\s*(?:[^' + matchCh + '\\\\]+|\\\\\\\\|\\\\.)' + matchCh;
-      if (stream.match(new RegExp(regex), true)) {
-        return tokenTypes.linkHref;
-      }
-    }
-
     // If this block is changed, it may need to be updated in GFM mode
     if (ch === '`') {
       var previousFormatting = state.formatting;
@@ -501,36 +448,6 @@ CodeMirror.defineMode("udon", function(cmCfg, modeCfg) {
     }
 
     if (ch === "*" || ch === "_") {
-/* ~udon toss
-      var len = 1, before = stream.pos == 1 ? " " : stream.string.charAt(stream.pos - 2)
-      while (len < 3 && stream.eat(ch)) len++
-      var after = stream.peek() || " "
-      // See http://spec.commonmark.org/0.27/#emphasis-and-strong-emphasis
-      var leftFlanking = !/\s/.test(after) && (!punctuation.test(after) || /\s/.test(before) || punctuation.test(before))
-      var rightFlanking = !/\s/.test(before) && (!punctuation.test(before) || /\s/.test(after) || punctuation.test(after))
-      var setEm = null, setStrong = null
-      if (len % 2) { // Em
-        if (!state.em && leftFlanking && (ch === "*" || !rightFlanking || punctuation.test(before)))
-          setEm = true
-        else if (state.em == ch && rightFlanking && (ch === "*" || !leftFlanking || punctuation.test(after)))
-          setEm = false
-      }
-      if (len > 1) { // Strong
-        if (!state.strong && leftFlanking && (ch === "*" || !rightFlanking || punctuation.test(before)))
-          setStrong = true
-        else if (state.strong == ch && rightFlanking && (ch === "*" || !leftFlanking || punctuation.test(after)))
-          setStrong = false
-      }
-      if (setStrong != null || setEm != null) {
-        if (modeCfg.highlightFormatting) state.formatting = setEm == null ? "strong" : setStrong == null ? "em" : "strong em"
-        if (setEm === true) state.em = ch
-        if (setStrong === true) state.strong = ch
-        var t = getType(state)
-        if (setEm === false) state.em = false
-        if (setStrong === false) state.strong = false
-        return t
-      }
-~udon toss */
 // ~udon start
       var setEm = null, setStrong = null
       if (ch === "_") setEm = !state.em // Em
@@ -655,14 +572,12 @@ CodeMirror.defineMode("udon", function(cmCfg, modeCfg) {
         formatting: false,
         linkText: false,
         linkHref: false,
-        linkTitle: false,
         code: 0,
         em: false,
         strong: false,
         // most recent tokenTypes.em or tokenTypes.strong
         lastEmOrStrong: null, // ~udon
         header: 0,
-        setext: 0,
         hr: false,
         list: false,
         listStack: [],
@@ -689,14 +604,12 @@ CodeMirror.defineMode("udon", function(cmCfg, modeCfg) {
         text: s.text,
         formatting: false,
         linkText: s.linkText,
-        linkTitle: s.linkTitle,
         linkHref: s.linkHref,
         code: s.code,
         em: s.em,
         strong: s.strong,
         lastEmOrStrong: s.lastEmOrStrong, // ~udon
         header: s.header,
-        setext: s.setext,
         hr: s.hr,
         list: s.list,
         listStack: s.listStack.slice(0),
